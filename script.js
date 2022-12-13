40747538784cdbc7aa9348dcc5cabfc48b046947
sohanisgreat <120393836+sohanisgreat@users.noreply.github.com>
2022-12-13 20:48:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+function logout() {
+    document.getElementById("bookPage").style.display = "none";
+    document.getElementById("loginPage").style.display = "block";
+}
+
+// -------------------------------------
+
+let users = [
+    {
+        uname: "soham",
+        pass: "soham10"
+    },
+    {
+        uname: "umesh",
+        pass: "umesh10"
+    },
+    {
+        uname: "somshekhar",
+        pass: "somshekhar"
+    }
+];
+
+function checkUser(uname, pass) {
+    for (let i = 0; i < users.length; i++) {
+        if(users[i].uname === uname && users[i].pass === pass)
+            return true;
+    }
+    return false;
+}
+
+function login() {
+    let uname = document.getElementById("username");
+    let pass = document.getElementById("password");
+
+    if (checkUser(uname.value, pass.value)) {
+        document.getElementById("bookPage").style.display = "block";
+        document.getElementById("loginPage").style.display = "none";
+        uname.value = '';
+        pass.value = '';
+    } else {
+        document.getElementById("error").style.display = "block";
+    }
+}
+
+// --------------------------------------------
+
 let inputArray = [];
 let booksObjectArray = [];
 
@@ -20,6 +66,12 @@ function addToLibrary() {
         book[inputArray[i].id] = inputArray[i].value;
     
     booksObjectArray.push(book);
+    document.getElementById("form-status").style.display = "block";
+
+    setTimeout(() => {
+        resetForm();
+        document.getElementById("form-status").style.display = "none";
+    }, 1200);
 }
 
 function resetForm() {
@@ -33,12 +85,15 @@ function resetForm() {
 function showLibrary() {
     inputArray = document.getElementsByClassName("validate");
     let html = `
-        <table border="1">
+        <h2 style="margin-bottom: 10px;">Library</h2>
+        <table border="1" class="table">
             <thead>
                 <tr>
+                    <td>S. No.</td>
                     <td>Book Name</td>
                     <td>Author Name</td>
-                    <td>Book Link</td>
+                    <td>Book PDF</td>
+                    <td>Book Cover</td>
                 </tr>
             </thead>
             <tbody>
@@ -47,9 +102,11 @@ function showLibrary() {
     for (let i = 0; i < booksObjectArray.length; i++) {
         html += `
                 <tr>
+                    <td>${i+1}</td>
                     <td>${booksObjectArray[i].title}</td>
                     <td>${booksObjectArray[i].author}</td>
-                    <td><a href="${booksObjectArray[i].link}">PDF</a></td>
+                    <td><a href="${booksObjectArray[i].pdflink}" target="_blank">Read PDF</a></td>
+                    <td><img src="${booksObjectArray[i].imglink}" style="width: 100px;"></td>
                 </tr>
         `;
     }
@@ -57,9 +114,39 @@ function showLibrary() {
     html += `
             </tbody>
         </table>
+        <div class="inputs">
+            <input type="button" value="Remove A Record" onclick="removeARecord()">
+            <input type="button" value="Remove All" onclick="removeAllRecords()">
+        </div>
     `;
 
     let library = document.getElementById("library-div");
     library.style.display = "block";
     library.innerHTML = html;
+}
+
+function removeARecord() {
+    if (booksObjectArray.length == 0) return alert("Library is empty");
+
+    let recordNum = prompt("Enter record S. No. to delete: ");
+
+    if (isNaN(recordNum))
+        return alert("Please enter a valid number.");
+
+    if (recordNum < 1 || recordNum > booksObjectArray.length)
+        return alert("Record does not exists.");
+
+    const index = booksObjectArray.indexOf(booksObjectArray[recordNum - 1]);
+    if (index > -1) {
+        if(confirm("Are you sure you want to remove this book?"))
+            booksObjectArray.splice(index, 1);
+    }
+    else
+        return alert("Record not found.");
+}
+
+function removeAllRecords() {
+    if (booksObjectArray.length == 0) return alert("Library is empty.");
+    if (confirm("Are you sure you want to delete all the records?"))
+        booksObjectArray = [];
 }
